3c4d63e13ade0705754a7bf50da7a21e30a0caf7
Forever <lynk0613@hotmail.com>
2018-10-09 22:39:26 -0400

    add button

---
+++ my-plinko3d/src/app/game/config.ts
@@ -0,0 +1,9 @@
+export const enum BasicParam {
+  offsetY = -200,
+  grids = 14,
+  diceSize = 20,
+  gridWidth = 40,
+  barWidth = 4,
+  backWidth = 1500,
+  backHeight = 1000
+}
--- my-plinko3d/src/app/game/game.component.ts
+++ my-plinko3d/src/app/game/game.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ElementRef, ViewChild, Input} from '@angular/core';
 import * as THREE from 'three-full';
+import {BasicParam} from './config';
+
 import {
   CameraComponentParams,
   App } from 'whs';
@@ -13,46 +15,15 @@ import * as PHYSICS from 'physics-module-ammonext'
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
-  private _container: App;
+	private _container: App;	
+	private dice: any;
+	fitstRoll = true;
+	
+
 	@ViewChild('rendererContainer') rendererContainer: ElementRef;
-  @Input() public camera: CameraComponentParams = {
-    position: {
-      y: 20,
-      z: 50
-    },
-  };
 
-  @Input() public rendering = {
-    bgColor: 0x162129,
-    renderer: {
-      antialias: true,
-      shadowmap: {
-        type: THREE.PCFSoftShadowMap,
-      },
-    }
-	};
 	basePath = window.location.href;
-
-	world = {
-		stats: 'fps', // fps, ms, mb or false if not need.
-		autoresize: 'window',
-	
-		gravity: [0, -1, 0],
-		rendering: {
-			background: {
-				color: 0x162129
-			},
-			pixelRatio: false,
-	
-			renderer: {
-				antialias: true
-			}
-		},
-	
-		shadowmap: {
-			type: THREE.PCFSoftShadowMap
-		}
-	};
+ 
 	appDefaults = {
 		camera: {
 			position: new THREE.Vector3(0, 0, 100),
@@ -85,37 +56,24 @@ export class GameComponent implements OnInit {
 		softbody: 0x434B7F
 	};
 
-	
-
-
 	constructor() { }
 	private createContainer() {
     this._container = new App([ 
 				new WHS.ElementModule(),
 				new WHS.SceneModule(),
-				new WHS.DefineModule('camera', new WHS.OrthographicCamera({	camera: {far: 1000	},position: {	z:500}})),
+				new WHS.DefineModule('camera', new WHS.OrthographicCamera({	camera: {far: 1000},position: {z:500}})),
 				new WHS.RenderingModule(this.appDefaults.rendering, {shadow: false}),
 				new PHYSICS.WorldModule(this.appDefaults.physics),
-				new WHS.OrbitControlsModule(),
+				// new WHS.OrbitControlsModule(),
 				// new StatsModule(),
 				// new WHS.ResizeModule()
 		]);
   }
 
+	
   public build() {
 
-		const offsetY = -200;
-		const grids = 14;
-		const diceSize = 20;
-		const gridWidth = 40;
-		const barWidth = 4;
-		const backWidth = 1500;
-		const backHeight = 1000;
 
-
-		// bar material
-
-		
 
 		// dice
 		const loader = new THREE.TextureLoader();
@@ -146,11 +104,11 @@ export class GameComponent implements OnInit {
 			})
 	 	];
 
-    const dice = new WHS.Box({ 
+    this.dice = new WHS.Box({ 
 			geometry: {
-				width: diceSize,
-				height: diceSize,
-				depth: diceSize
+				width: BasicParam.diceSize,
+				height: BasicParam.diceSize,
+				depth: BasicParam.diceSize
 			},
 	
 			modules: [
@@ -162,26 +120,26 @@ export class GameComponent implements OnInit {
 			],
 	
 			material: diceMaterials,//new THREE.MeshPhongMaterial({color: 0x447F8B}),
-			position: new THREE.Vector3(- gridWidth / 2 + (5 * Math.random() * (Math.random()>.5?1:-1)), gridWidth * (grids + 1) * Math.sin(Math.PI / 3) + offsetY, 0)
+			position: new THREE.Vector3(- BasicParam.gridWidth / 2 + (5 * Math.random() * (Math.random()>.5?1:-1)), BasicParam.gridWidth * (BasicParam.grids + 1) * Math.sin(Math.PI / 3) + BasicParam.offsetY, 0)
 		});
 
-		dice.addTo(this._container);
-
-		dice.on('collision', (otherObject, v, r, contactNormal) => {
+		this.dice.on('collision', (otherObject, v, r, contactNormal) => {
 			if (otherObject.component.modules[0].data.type === 'cylinder') {
-				otherObject.material.color.setHex(0xff0000)
-				// otherObject.material.color = 0xff0000;
+				otherObject.material.color.setHex(0xffffff)
+				setTimeout(()=>{
+					otherObject.material.color.setHex(0x447F8B)
+				}, 3000);
 			}
 		});
 
 	
-		for (let i = grids; i > 0; i--) {
+		for (let i = BasicParam.grids; i > 0; i--) {
 			for (let j = 0; j < i; j++ ) {
 				new WHS.Cylinder({
 					geometry: {
-						radiusTop: barWidth,
-						radiusBottom: barWidth,
-						height: gridWidth
+						radiusTop: BasicParam.barWidth,
+						radiusBottom: BasicParam.barWidth,
+						height: BasicParam.gridWidth
 					},
 				
 					material: new THREE.MeshBasicMaterial({
@@ -199,17 +157,17 @@ export class GameComponent implements OnInit {
 					},
 					
 					position: {
-						x: j * gridWidth - gridWidth * i / 2,
-						y: gridWidth * Math.sin(Math.PI/3) * (grids - i + 1) + offsetY
+						x: j * BasicParam.gridWidth - BasicParam.gridWidth * i / 2,
+						y: BasicParam.gridWidth * Math.sin(Math.PI/3) * (BasicParam.grids - i + 1) + BasicParam.offsetY
 					}
 				}).addTo(this._container);
 			}
 
  			new WHS.Box({ 
 				geometry: {
-					width: barWidth / 2,
-					height: gridWidth * Math.sin(Math.PI / 3),
-					depth: gridWidth
+					width: BasicParam.barWidth / 2,
+					height: BasicParam.gridWidth * Math.sin(Math.PI / 3),
+					depth: BasicParam.gridWidth
 				},
 		
 				modules: [
@@ -225,8 +183,8 @@ export class GameComponent implements OnInit {
 				}),
 
 				position: {
-					y: offsetY + gridWidth * Math.sin(Math.PI/3) / 2,
-					x: (i - grids / 2 - 1) * gridWidth
+					y: BasicParam.offsetY + BasicParam.gridWidth * Math.sin(Math.PI/3) / 2,
+					x: (i - BasicParam.grids / 2 - 1) * BasicParam.gridWidth
 				}
 			}).addTo(this._container);
 		}
@@ -235,9 +193,9 @@ export class GameComponent implements OnInit {
 		// back
 		new WHS.Box({ 
 			geometry: {
-				width: backWidth,
-				height: backHeight,
-				depth: barWidth 
+				width: BasicParam.backWidth,
+				height: BasicParam.backHeight,
+				depth: BasicParam.barWidth 
 			},
 	
 			modules: [
@@ -251,7 +209,7 @@ export class GameComponent implements OnInit {
 			}),
 
 			position: {
-				z: - gridWidth / 2,
+				z: - BasicParam.gridWidth / 2,
 				y: 0
 			}
 		}).addTo(this._container);
@@ -259,8 +217,8 @@ export class GameComponent implements OnInit {
 		// front
 		new WHS.Box({ 
 			geometry: {
-				width: backWidth,
-				height: backHeight,
+				width: BasicParam.backWidth,
+				height: BasicParam.backHeight,
 				depth: 0
 			},
 	
@@ -277,7 +235,7 @@ export class GameComponent implements OnInit {
 			}),
 
 			position: {
-				z: gridWidth / 2 + .1,
+				z: BasicParam.gridWidth / 2 + .1,
 				y: 0
 			}
 		}).addTo(this._container);
@@ -285,9 +243,9 @@ export class GameComponent implements OnInit {
 		// bottom
     new WHS.Box({ 
 			geometry: {
-				width: gridWidth * (grids + 2),
-				height: barWidth / 2,
-				depth: gridWidth
+				width: BasicParam.gridWidth * (BasicParam.grids + 2),
+				height: BasicParam.barWidth / 2,
+				depth: BasicParam.gridWidth
 			},
 	
 			modules: [
@@ -301,17 +259,17 @@ export class GameComponent implements OnInit {
 			}),
 
 			position: {
-				y: offsetY,
-				x: - gridWidth / 2
+				y: BasicParam.offsetY,
+				x: - BasicParam.gridWidth / 2
 			}
 		}).addTo(this._container);
 
 		// left
     new WHS.Box({ 
 			geometry: {
-				width: gridWidth * (grids + 2),
-				height: barWidth / 2,
-				depth: gridWidth
+				width: BasicParam.gridWidth * (BasicParam.grids + 2),
+				height: BasicParam.barWidth / 2,
+				depth: BasicParam.gridWidth
 			},
 	
 			modules: [
@@ -325,8 +283,8 @@ export class GameComponent implements OnInit {
 			}),
 
 			position: {
-				x: -gridWidth * Math.sin(Math.PI/3) * (grids / 3 + 0.5) ,
-				y: offsetY + gridWidth * (grids + 2) / 2 * Math.sin(Math.PI/3)
+				x: -BasicParam.gridWidth * Math.sin(Math.PI/3) * (BasicParam.grids / 3 + 0.5) ,
+				y: BasicParam.offsetY + BasicParam.gridWidth * (BasicParam.grids + 2) / 2 * Math.sin(Math.PI/3)
 			},
 
 			rotation: {
@@ -337,9 +295,9 @@ export class GameComponent implements OnInit {
 		// right
     new WHS.Box({ 
 			geometry: {
-				width: gridWidth * (grids + 2),
-				height: barWidth / 2,
-				depth: gridWidth
+				width: BasicParam.gridWidth * (BasicParam.grids + 2),
+				height: BasicParam.barWidth / 2,
+				depth: BasicParam.gridWidth
 			},
 	
 			modules: [
@@ -353,8 +311,8 @@ export class GameComponent implements OnInit {
 			}),
 
 			position: {
-				x: gridWidth * Math.sin(Math.PI / 3) * (grids / 3 - 0.5) - barWidth ,
-				y: offsetY + gridWidth * (grids + 2) / 2 * Math.sin(Math.PI/3)
+				x: BasicParam.gridWidth * Math.sin(Math.PI / 3) * (BasicParam.grids / 3 - 0.5) - BasicParam.barWidth ,
+				y: BasicParam.offsetY + BasicParam.gridWidth * (BasicParam.grids + 2) / 2 * Math.sin(Math.PI/3)
 			},
 
 			rotation: {
@@ -385,48 +343,23 @@ export class GameComponent implements OnInit {
 
     // Start the app
 		this._container.start();
-		// new WHS.Loop(() => {
-		// 	box.rotation.y += 0.02;
-		// }).start(this._container);
+ 
   }
 
   ngOnInit() {
     this.createContainer();
     this.build();
-  }
-
- 
+	}
+	
+	dropDice() {
+ 		if (this.fitstRoll) {
+			this.dice.addTo(this._container);
+			this.fitstRoll = false;
+		} 
+		this.dice.position = new THREE.Vector3(- BasicParam.gridWidth / 2 + (5 * Math.random() * (Math.random()>.5?1:-1)), BasicParam.gridWidth * (BasicParam.grids + 1) * Math.sin(Math.PI / 3) + BasicParam.offsetY, 0)
+ 	}
 }
 
- 		// const teapot = new WHS.Importer({
-		// 	url: `assets/model/utah-teapot-light.json`,
-		
-		// 	modules: [
-		// 		new PHYSICS.ConcaveModule({
-		// 			friction: 0.5,
-		// 			mass: 1,
-		// 			restitution: 0.5,
-		// 			path: `assets/model/utah-teapot-light.json`,
-		// 			scale: new THREE.Vector3(1, 2, 1)
-		// 		}),
-		// 		new WHS.TextureModule({
-		// 			url: `assets/dice.png`,
-		// 			repeat: new THREE.Vector2(1, 1)
-		// 		})
-		// 	],
-		
-		// 	useCustomMaterial: true,
-		
-		// 	material: new THREE.MeshPhongMaterial({
-		// 		shading: THREE.SmoothShading,
-		// 		side: THREE.DoubleSide
-		// 	}),
-		
-		// 	position: {
-		// 		y: 200
-		// 	},
-		
-		// 	scale: [1,1,1]
-		// });
-
-		// teapot.addTo(this._container);
+ 
+ 
+ 
